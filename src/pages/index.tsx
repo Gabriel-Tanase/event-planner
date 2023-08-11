961c98b513d5a2da924a658acc0081434e9b5611
Gabriel Tanase <gabrieltanasedev@gmail.com>
2023-08-11 12:03:32 +0300

    Add hotjar for test miller

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,21 @@ import Layout from "@/Layouts/Layout";
 import { getVerifyFetcher } from "@/features/Authentication/service/fetcher";
 import { useAuthenticationService } from "@/features/Authentication/service/useAuthenticationService";
 import { useGetCurrentUser } from "@/features/UserProfile/service/useUserService";
+import { useEffect } from "react";
+import Hotjar from "@hotjar/browser";
+
+const siteId = 3610112;
+const hotJarVersion = 6;
 
 export default function Home() {
 	const { useVerifyLoggedIn } = useAuthenticationService();
 	const { isUserAuthenticated } = useVerifyLoggedIn();
 
 	useGetCurrentUser(isUserAuthenticated);
+
+	useEffect(() => {
+		Hotjar.init(siteId, hotJarVersion);
+	}, []);
 
 	return (
 		<Layout>
